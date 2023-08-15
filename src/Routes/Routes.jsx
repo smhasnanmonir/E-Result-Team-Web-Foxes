0df464d49780adbfdf5460f42a59c71a151a3be9
Nabil-Hossain-NBL <nabilhossainprf@gmail.com>
2023-08-15 11:37:47 +0600

    result page created

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import HomePage from "../components/HomePage/HomePage";
+import InformationPage from "../pages/InformationPage/InformationPage";
 
 export const router = createBrowserRouter([
   {
@@ -11,6 +12,14 @@ export const router = createBrowserRouter([
         path: "/",
         element: <HomePage></HomePage>
       },
+      {
+        path: "information-page",
+        element: <InformationPage></InformationPage>
+      },
+      {
+        path: "result-page",
+        element: <InformationPage></InformationPage>
+      }
     ],
   },
 ]);
---
+++ src/pages/ResultPage/ResultPage.jsx
@@ -0,0 +1,10 @@
+
+const ResultPage = () => {
+    return (
+        <div>
+            <h2>hi</h2>
+        </div>
+    );
+};
+
+export default ResultPage;
