fabbb037ee83053bebfa5a1045e1885f911d0280
S.M Hasnan Monir <114436561+smhasnanmonir@users.noreply.github.com>
2023-08-17 19:43:56 +0600

    Revert "Nabil hossain nbl"

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,11 +1,42 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "../components/HomePage/HomePage";
-import InformationPage from "../pages/InformationPage/InformationPage";
-import ResultPage from "../pages/ResultPage/ResultPage";
 import Dashboard from "../components/Dashborad/Dashboard";
 import AddResult from "../components/AddResult/AddResult";
 import ViewAll from "../components/ViewAll/ViewAll";
 import Header from "../components/Header/Header";
+
+// export const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Main></Main>,
+//     children: [
+//       {
+//         path: "/",
+//         element: <HomePage></HomePage>
+//       },
+//       {
+//         path:'dashboard',
+//         element:<Dashboard></Dashboard>,
+//         children:[
+//           {
+//             path:'addResult',
+//             element:<AddResult></AddResult>
+//           },
+//           {
+//             path:'viewAll',
+//             element:<ViewAll></ViewAll>
+//           }
+
+//         ]
+//     },
+//     ],
+//   },
+// ]);
+
+// 
+ 
+
+// 
 export const router = createBrowserRouter([
   {
     path: "/",
@@ -15,24 +46,7 @@ export const router = createBrowserRouter([
         path:'/',
         element: <HomePage></HomePage>
       },
-  ,
-  {
-    path: '/login',
-    element: <Login></Login>
-  },
-  {
-    path: '/signup',
-    element: <Signup></Signup>
-  },
       {
-        path: "information-page",
-        element: <InformationPage></InformationPage>
-      },
-      {
-        path: "result-page",
-        element: <ResultPage></ResultPage>
-      }
-    ],
           path:'dashboard',
           element:<Dashboard></Dashboard>,
           children:[
