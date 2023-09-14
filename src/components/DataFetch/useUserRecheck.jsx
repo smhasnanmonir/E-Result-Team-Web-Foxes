416ff23fd72b28b6ff044f2495517dfa31f003f7
useless444 <habibhk127@gmail.com>
2023-09-15 00:37:15 +0600

    Notification

--- src/components/DataFetch/useUserRecheck.jsx
+++ src/components/DataFetch/useUserRecheck.jsx
@@ -1,19 +1,32 @@
 import { useContext } from "react";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import { AuthContext } from "../Account/Provider/AuthProvider";
+import { useQuery } from "@tanstack/react-query";
 
 const useUserRecheck = () => {
+  
   const {user} = useContext(AuthContext);
   // console.log(user);
-  const [userRecheckAll, setallRecheck] = useState([]);
-  useEffect(() => {
-    fetch(`https://e-result-server.vercel.app/reCheckUser?email=${user.email}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setallRecheck(data);
-      });
-  }, [user.email]);
-  return [userRecheckAll];
+  const { refetch, data: reCheckUser = [] } = useQuery({
+    queryKey: ["reCheckUser"],
+    queryFn: async () => {
+      const res = await fetch(`https://e-result-server.vercel.app/reCheckUser?email=${user.email}`);
+      return res.json();
+    },
+  });
+
+  return [reCheckUser, refetch];
+  // const {user} = useContext(AuthContext);
+  // // console.log(user);
+  // const [userRecheckAll, setallRecheck] = useState([]);
+  // useEffect(() => {
+  //   fetch(`https://e-result-server.vercel.app/reCheckUser?email=${user.email}`)
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setallRecheck(data);
+  //     });
+  // }, [user.email]);
+  // return [userRecheckAll];
 };
 
 export default useUserRecheck;
