b816c83a7d07cdfe1634e606f2f16e76d3a5f8ec
useless444 <habibhk127@gmail.com>
2023-09-18 22:53:24 +0600

    Implemented Redux

--- src/main.jsx
+++ src/main.jsx
@@ -10,20 +10,24 @@ import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query'
+import { Provider } from "react-redux";
+import store from "./components/DataFetch/Redux/store";
 
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <QueryClientProvider client={queryClient}>
-        <div className="mx-auto max-w-screen-2xl">
-        <HelmetProvider>
-          <RouterProvider router={router} />
-        </HelmetProvider>
-        </div>
-      </QueryClientProvider>
-    </AuthProvider>
+    <Provider store={store}>
+      <AuthProvider>
+        <QueryClientProvider client={queryClient}>
+          <div className="mx-auto max-w-screen-2xl">
+          <HelmetProvider>
+            <RouterProvider router={router} />
+          </HelmetProvider>
+          </div>
+        </QueryClientProvider>
+      </AuthProvider>
+    </Provider>
   </React.StrictMode>
 );
